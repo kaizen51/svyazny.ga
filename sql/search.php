d753afb42cde626daaf0b22f56c613cae8502a61
Michael <48417487+kaizen51@users.noreply.github.com>
2019-04-25 08:45:42 +0300

    Update search.php

--- sql/search.php
+++ sql/search.php
@@ -1,19 +1,4 @@
 <?php
-/**
-*
-* This file is part of the phpBB Forum Software package.
-*
-* @copyright (c) phpBB Limited <https://www.phpbb.com>
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
-* For full copyright and license information, please see
-* the docs/CREDITS.txt file.
-*
-*/
-
-/**
-* @ignore
-*/
 define('IN_PHPBB', true);
 $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
